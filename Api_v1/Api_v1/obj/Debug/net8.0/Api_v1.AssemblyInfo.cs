6cc26f17a4e855e81b734b3fdd36c8331fb4b912
ESM-7 <ezelivevil09@gmail.com>
2025-03-12 19:55:30 -0600

    v1
    cambios

--- Api_v1/Api_v1/obj/Debug/net8.0/Api_v1.AssemblyInfo.cs
+++ Api_v1/Api_v1/obj/Debug/net8.0/Api_v1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3001dd4215dcab2aba8e0f52553eac21e8af228c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
